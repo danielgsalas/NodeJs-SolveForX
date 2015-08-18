3f30f0f3381458127c205882640b01d4cb1d8985
danielgsalas <danielgsalas@yahoo.com>
2015-08-17 17:05:23 -0700

    added routes to add, subtract, multiply and divide input numbers

--- server.js
+++ server.js
@@ -26,6 +26,121 @@ router.get('/', function(req, res) {
 
 // more routes for our API will happen here
 
+// find sum of x and y
+// example: http://localhost:8080/api/sum/2/3
+router.route("/sum/:x/:y")
+
+	.get(function(req, res) {
+
+		try {
+			var x = parseFloat(req.params.x);
+			var y = parseFloat(req.params.y);
+			
+			if (x == null || isNaN(x) ||
+				(x+"").length < req.params.x.length) {
+				res.json({ error: "Failed to parse " + req.params.x });
+			}
+			else if (y == null || isNaN(y) ||
+					(y+"").length < req.params.y.length) {
+				res.json({ error: "Failed to parse " + req.params.y });
+			}
+			else {
+				var sum = x + y;
+				res.json({ sum : sum });
+			}			
+		}
+		catch (error) {
+			res.json({ error : error.message })
+		}
+	});
+
+// subtract subtrahend from minuend
+// example: http://localhost:8080/api/difference/9/4
+router.route("/difference/:minuend/:subtrahend")
+
+	.get(function(req, res) {
+		
+		try {
+			var minuend = parseFloat(req.params.minuend);
+			var subtrahend = parseFloat(req.params.subtrahend);
+			
+			if (minuend == null || isNaN(minuend) ||
+			   (minuend+"").length < req.params.minuend.length) {
+				res.json({ error: "Failed to parse " + req.params.minuend });
+			}
+			else if (subtrahend == null || isNaN(subtrahend) ||
+				    (subtrahend+"").length < req.params.minuend.subtrahend) {
+				res.json({ error: "Failed to parse " + req.params.minuend });
+			}
+			else {
+				var difference = minuend - subtrahend;
+				res.json({ difference : difference });
+			}			
+		}
+		catch (error) {
+			res.json({ error : error.message })
+		}
+	});
+
+// find product of multiplying x and y
+// example: http://localhost:8080/api/product/2/3
+router.route("/product/:x/:y")
+
+	.get(function(req, res) {
+		
+		try {
+			var x = parseFloat(req.params.x);
+			var y = parseFloat(req.params.y);
+			
+			if (x == null || isNaN(x) ||
+		       (x+"").length < req.params.x.length) {
+				res.json({ error: "Failed to parse " + req.params.x });
+			}
+			else if (y == null || isNaN(y) ||
+					(y+"").length < req.params.y.length) {
+				res.json({ error: "Failed to parse " + req.params.y });
+			}
+			else {
+				var product = x * y;
+				res.json({ product : product });
+			}			
+		}
+		catch (error) {
+			res.json({ error : error.message })
+		}
+	});
+
+// find quotient by dividing dividend by divisor
+// example: http://localhost:8080/api/quotient/12/3
+router.route("/quotient/:dividend/:divisor")
+
+	.get(function(req, res) {
+		
+		try {
+			var dividend = parseFloat(req.params.dividend);
+			var divisor = parseFloat(req.params.divisor);
+			
+			if (dividend == null || isNaN(dividend) ||
+			   (dividend+"").length < req.params.dividend.length) {
+				res.json({ error: "Failed to parse " + req.params.dividend });
+			}
+			else if (divisor == null || isNaN(divisor) ||
+				    (divisor+"").length < req.params.divisor.length) {
+				res.json({ error: "Failed to parse " + req.params.divisor });
+			}
+			else if (divisor == 0) {
+				res.json({ quotient: "undefined" });
+			}
+			else {
+				var quotient = dividend / divisor;
+				res.json({ quotient : quotient });
+			}			
+		}
+		catch (error) {
+			res.json({ error : error.message })
+		}
+	});
+
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
@@ -33,4 +148,4 @@ app.use('/api', router);
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('Listening on port ' + port);
